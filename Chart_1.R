2eb5c2619a9008987d513696e8bdaccbb0b5607e
samkaing <samkaing@uw.edu>
2023-02-20 20:33:58 -0800

    samsgraph+chart1

--- Chart_1.R
+++ Chart_1.R
@@ -2,10 +2,8 @@
 
 maternal_risk <- read.csv("Maternal Health Risk Data Set (1).csv", stringsAsFactors = FALSE)
 
-maternal_risk <- maternal_risk %>% 
-  mutate(person = row_number(Age))
 library("ggplot2")
 library("dplyr")
 
-ggplot(maternal_risk) + geom_count(aes(x = person, y = BS, color = RiskLevel))
+ggplot(maternal_risk) + geom_count(aes(x = RiskLevel, y = BS, color = RiskLevel))
 

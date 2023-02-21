817ca43ff892c4b17df141aa7177678be4f9c6e0
jenineshafi <jenineshafi@gmail.com>
2023-02-20 19:49:27 -0800

    Updates to chart 3

--- chart_3_age_risk.R
+++ chart_3_age_risk.R
@@ -1,61 +1,21 @@
 # Age Distribution among different Risk Levels  
 
+# Load the dataset 
 maternal_risk <- read.csv("Maternal Health Risk Data Set (1).csv", stringsAsFactors = FALSE)
 
+# Load dplyr and ggplot
 library("ggplot2")
 library("dplyr")
 
-maternal_risk <- maternal_risk %>% 
-                 mutate(person = row_number(Age))
-
+# Create a boxplot showing age ditributions for high, mid, and low risk levels
 ggplot(maternal_risk, aes(x = RiskLevel, y = Age)) + 
   geom_boxplot(aes(color = RiskLevel)) +
   labs(title = "Age Distribution within each Risk Level Group",
        x = "Risk Level", 
        y = "Age")
 
-df <- maternal_risk %>% 
-      select(Age, RiskLevel)
 
-tab <- table(df %>% group_by(Age, RiskLevel) %>% 
-               summarize(mean = mean(Age)))
 
-View(tab)
-
-summary_stats <- list()
-summary_info$num_rows <- nrow(maternal_risk)
-summary_info$max_Age <- maternal_risk %>% 
-    filter(Age == max(Age, na.rm = TRUE))
-
-tab <- table(summary_info)
-
-summary_info <- list()
-summary_info$min_Age <- maternal_risk %>% 
-  filter(Age == min(Age, na.rm = TRUE)) %>% 
-  select(Age)
-summary_info$max_Age <- maternal_risk %>% 
-  filter(Age == max(Age, na.rm = TRUE)) %>% 
-  select(Age)
-summary_info$avg_age <- maternal_risk %>% 
-  select(Age) %>% 
-  summarize(mean = mean(Age))
-summary_info$avg_BS <- maternal_risk %>% 
-  select(BS) %>% 
-  summarize(mean = mean(BS))
-summary_info$avg_HeartRate <- maternal_risk %>% 
-  select(HeartRate) %>% 
-  summarize(mean = mean(HeartRate))
-summary_info$RiskLevel <- maternal_risk %>% 
-  select(RiskLevel) %>% 
-
-library("knitr")
-
-kable(summary_info,
-      col.names = c('Min Age',
-                   'Max Age',
-                   'Mean Age',
-                   'Blood Sugar', 
-                   'Heart Rate'))
 
 
 
